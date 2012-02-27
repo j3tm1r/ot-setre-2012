716ea54b21f85a2d2acc242b7abf1574d312d251
mihai.badau@gmail.com <mihai.badau@gmail.com@8de690aa-ab9c-6926-54ac-61cbbd2d4413>
2012-02-27 08:47:54 +0000

    first

---
+++ trunk/radio/conf.sh
@@ -0,0 +1,7 @@
+PATH=/usr/local/msp430-pytools/bin:$PATH
+PATH=/usr/local/msp430-gcc-4.4.4/bin:$PATH
+
+LD_LIBRARY_PATH=/usr/local/msp430-pytools/lib:$LD_LIBRARY_PATH
+
+export PATH
+export LD_LIBRARY_PATH
